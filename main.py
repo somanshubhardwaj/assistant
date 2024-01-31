9e1d8ddce7a66228c33d0f4d36f556b5697ba191
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-01-31 15:42:01 +0530

    Add auto import settings and update workspace.xml

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import speech_recognition as sr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from googletrans import Translator
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def listen():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, I couldn't understand what you said.")
+        return None
+    except sr.RequestError as e:
+        print(f"Sorry, there was an error while requesting results; {e}")
+        return None
+
+
+"""while True:
+    text = listen()
+    if text:
+        print("You said:", text)
+        break
+"""
+
+langcode = {"Hindi": "hi", "English": "en", "French": "fr", "German": "de", "Spanish": "es", "Italian": "it",
+            "Japanese": "ja", "Korean": "ko", "Russian": "ru", "Chinese": "zh-cn"}
+
+
+def translate_text(text, target_language='en'):
+    translator = Translator()
+    translated_text = translator.translate(text, dest=target_language)
+    return translated_text.text
+
+
+text_to_translate = input("Enter the text to translate: ")
+print("Languages:")
+for language in langcode:
+    print(f"{language} ({langcode[language]})")
+target_language = input("Enter the target language (e.g., 'fr' for French): ")
+translated_text = translate_text(text_to_translate, target_language)
+print("Translated text:", translated_text)
